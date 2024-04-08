239ea774d4e2a751ad33e689221f4d6da711a69f
gymcoding <bruce.lean17@gmail.com>
2024-04-08 09:08:32 +0900

    24_스크롤시 메뉴 Active 추가

--- js/main.js
+++ js/main.js
@@ -14,3 +14,29 @@ function init() {
 }
 
 init();
+
+const options = {
+  threshold: 1,
+};
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    const sectionId = entry.target.id;
+    if (entry.isIntersecting) {
+      document
+        .querySelector(`.nav__link[href*=${sectionId}]`)
+        .classList.add('active-link');
+
+      const $items = document.querySelectorAll(
+        `.nav__link:not([href*=${sectionId}])`,
+      );
+      $items.forEach((el) => el.classList.remove('active-link'));
+    }
+  });
+}, options);
+
+const $sectionList = document.querySelectorAll('.section');
+$sectionList.forEach((el) => observer.observe(el));
+// observer.observe($workSection);
+// observer.observe($workSection);
+// observer.observe($workSection);
